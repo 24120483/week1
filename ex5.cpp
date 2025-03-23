9b3e3083a10eb1f416e8ea96f06333014dc9d46c
24120483 <hoangvantruong080206@gmail.com>
2025-03-23 18:44:01 +0700

    week1

--- ex5.cpp
+++ ex5.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int sorted(int* arr, int n) {
-	if (n == 0 || n == 1) {
+	if (n == 0 || n == 1) {		//khi mang co 0 hoac 1 phan tu thi luon dung
 		return 1;
 	}
-	if (arr[n - 1] < arr[n - 2]) {
+	if (arr[n - 1] < arr[n - 2]) {		//neu phan tu cuoi cung cua mang nho hon phan tu truoc do thi sai
 		return 0;
 	}
-	return sorted(arr, n - 1);
+	return sorted(arr, n - 1);		//goi de quy ham voi n-1 phan tu con lai
 }
 int main() {
 	int n;
